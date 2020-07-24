1aa977dcebff9319f5df6151c55d79db6a274626
Philip Warren <philwarren1@gmail.com>
2020-07-24 15:50:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,16 @@ def line(array)
    return
   "The line is currently empty"
 else
-  return take_a 
+  return take_a_number(array) 
+end
+
+def take_a_number(katz_deli, array)
+end
+
+def now_serving(katz_deli_array)
+  if array == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "#{array}"
+end
 end
